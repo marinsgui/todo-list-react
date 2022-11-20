d5dafc35725815b9953baa568175f604fa16af2c
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-11-20 12:29:00 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Form from "./components/Form"
+
+import './App.css';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>Lista de tarefas</h1>
+      <Form />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Form/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+export default function Form() {
+    return (
+        <form>
+            <input type="text" name="input-task" id="input-task" placeholder='Insira a tarefa aqui' />
+        </form>
+    )
+}
