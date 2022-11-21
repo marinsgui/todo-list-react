d20b8383359c261165e5a2c39cd9e5fb49431b38
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-11-21 12:10:32 -0300

    Lista concluída

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,55 @@
-import Form from "./components/Form";
-import Task from "./components/Task";
-
 import './App.css';
 
+import { FaTrash } from 'react-icons/fa';
+import { useState } from 'react';
 
 
 function App() {
+
+  const [newTask, setNewTask] = useState('');
+  const [list, setList] = useState([]);
+
+  function addTask(e) {
+    e.preventDefault()
+    setList([...list, newTask]);
+    setNewTask('')
+  }
+
+  function deleteTask(index) {
+    let newList = [...list];
+    newList.splice(index, 1);
+    setList(newList);
+  }
+
+  function clearList() {
+    setList([])
+  }
   
   return (
     <div className="App">
       <h1>Lista de tarefas</h1>
-      <Form />
+        <form>
+              <input 
+              type="text" 
+              name="input-task" 
+              id="input-task" 
+              placeholder='Insira a tarefa aqui'
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)} 
+              />
+              <button className='add-btn' onClick={addTask}>Adicionar tarefa</button>
+        </form>
       <ul className="task-list">
-        <Task />
+        {list.map((item, index) => (
+          <li key={index}>
+            <p>{item}</p>
+            <button title="Apagar esta tarefa" onClick={() => deleteTask(index)}><FaTrash /></button>
+          </li>
+        ))}
       </ul>
+      {list.length !== 0 && (
+        <button className='clear-btn' onClick={clearList}>Limpar lista</button>
+      )}
     </div>
   )
 }
--- src/components/Form/index.jsx
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-
-export default function Form() {
-    return (
-        <form>
-            <input type="text" name="input-task" id="input-task" placeholder='Insira a tarefa aqui' />
-        </form>
-    )
-}
